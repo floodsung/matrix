5672e658e354b6a66eba5818b4e5679c07233791
刘新新 <liuxinxin@jszr.com>
2026-04-08 10:28:00 +0800

    content clear

--- scripts/build_mc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-cd src/jszr_mc
-./build.sh s
-
-echo "MC build complete."
--- scripts/build_navigo.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-set -e
-
-cd  src/navigo
-
-colcon build --packages-select pub_tf robots_dog_msgs
-
-echo "navigo package complete."
